59f25bffbcc37b4e647576219c78df1346c0143f
Jinwoo <zvzv9808@gmail.com>
2022-03-27 15:15:26 +0900

    Changes

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,24 +16,45 @@ public class JpaMain {
         //code 작성
         try{
 
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+
             Member member1 = new Member();
             member1.setUsername("member1");
+            member1.setTeam(teamA);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(teamB);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("m1 = "+refMember.getClass());
+//            Member m = em.find(Member.class, member1.getId());
+//
+//            System.out.println("m = " + m.getTeam().getClass());
 
-            Member findMember = em.find(Member.class, member1.getId());
-            System.out.println("reference = "+findMember.getClass());
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
+                            .getResultList();
 
-            System.out.println("refMember == findMember : "+(refMember == findMember));
+            System.out.println("========================");
+            member1.getTeam().getName(); //초기화
+            System.out.println(member1.getTeam().getName());
+            member2.getTeam().getName(); //초기화
+            System.out.println(member2.getTeam().getName());
+            System.out.println("========================");
 
             tx.commit();
         }catch (Exception e){
             tx.rollback();
+            e.printStackTrace();
         }finally{
             em.close();
         }
